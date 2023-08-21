ac0bfc7db1ae479eef5a3ad75507ff09c2ce3079
evobsidianops <cobrielisuekebho9270@gmail.com>
2023-08-21 13:51:06 -0700

    main printf logiic

---
+++ printf.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * printf - prints all argument passed to it
+ * format: its the character string
+ * Description: Prints all elements passes into it and also takes
+ * care of foramt specifiers
+ *
+ * Return: returns the input to std output
+ */
+int _printf(const char *format, ...)
+{
+	va_list parameters;
+	const char *i;
+	int j;
+	char *str;
+
+	va_start(parameters, format);
+	for (i = format; *i; i++)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '%')
+			{
+				_printchar('%');
+			}
+			else if (*format == 'c')
+			{
+				j = va_arg(parameters, int);
+				_printchar(j);
+			}
+			else if (*format == 's')
+			{
+				str = va_arg(parameters, char*);
+				_putschar(str);
+			}
+		}
+		else
+		{
+			_printchar(*format);
+		}
+		format++;
+	}
+	va_end(parameters);
+	return (0);
+}
