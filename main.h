e14e382c271a42f4b5179968586fb3c718edfdc2
evobsidianops <cobrielisuekebho9270@gmail.com>
2023-08-24 12:31:34 +0100

    hot fix

--- main.h
+++ main.h
@@ -29,5 +29,7 @@ int _printf(const char *format, ...);
 int print_int(va_list parameters, int length);
 int print_str(va_list parameters, int length);
 specifier_function all_functions(char specifier);
+int print_integer(va_list parameters, int length);
+int print_digit(va_list parameters, int length);
 
 #endif
